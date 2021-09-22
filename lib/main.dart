bad416afd9cc09507613a0a7ac0ef51ea5547c16
hyuntn0724 <hyuntn0724@gmail.com>
2021-09-22 16:10:47 +0900

    delete commented code

--- lib/main.dart
+++ lib/main.dart
@@ -17,25 +17,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-// class MyHomePage extends StatelessWidget {
-//   final String title;
-//
-//   MyHomePage({Key? key, required this.title}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(this.title),
-//       ),
-//       body: Center(
-//           child:
-//           Text(
-//             '메인 화면',
-//           )
-//       ),
-//     );
-//   }
-// }
